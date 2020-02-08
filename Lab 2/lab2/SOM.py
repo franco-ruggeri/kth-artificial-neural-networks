1689dad870ffb8e0e4408557cb471c4b33a3de31
Franco Ruggeri <fruggeri@kth.se>
2020-02-08 02:44:08 +0100

    Complete part 2

--- Lab 2/lab2/SOM.py
+++ Lab 2/lab2/SOM.py
@@ -2,67 +2,98 @@ import numpy as np
 import math
 
 
+def manhattan(x, y):
+    """Manhattan distance."""
+    return abs(x[0] - y[0]) + abs(x[1] - y[1])
+
+
+def _neighbourhood_1d(winner, n_nodes, neighbourhood_size, circular):
+    """Get neighbourhood, represented as indexes, of a node. The node itself is included. 1D version."""
+    idx_min, idx_max = math.ceil(winner - neighbourhood_size), math.floor(winner + neighbourhood_size)
+    if circular:
+        if idx_max >= n_nodes:
+            neighbourhood = np.arange(idx_min, n_nodes)  # [idx_min, n_nodes-1] plus...
+            idx_max %= n_nodes
+            neighbourhood = np.concatenate((neighbourhood, np.arange(idx_max + 1)))  # ... [0, idx_max % n_nodes]
+        else:
+            neighbourhood = np.arange(idx_min, idx_max + 1)
+    else:
+        if idx_min < 0:
+            idx_min = 0
+        if idx_max >= n_nodes:
+            idx_max = n_nodes - 1
+        neighbourhood = np.arange(idx_min, idx_max + 1)
+    return neighbourhood
+
+
+def _neighbourhood_2d(winner, side, neighbourhood_size):
+    """Get neighbourhood, represented as indexes, of a node. The node itself is included. 2D version."""
+    return np.array([(i, j) for j in range(side) for i in range(side)
+                     if manhattan(winner, (i, j)) <= neighbourhood_size])
+
+
 def _select_distance(distance):
+    """Select distance function for pattern and nodes (input space)."""
     if distance == 'euclidean':
-        return lambda x, y: (x-y).dot(x-y)  # no square root, not necessary
+        return lambda x, y: (x - y).dot(x - y)  # no square root, not necessary
+
+
+def _select_neighbourhood(dim, n_nodes, circular):
+    """Select neighbourhood function depending on the dimensionality of the output space."""
+    if dim == 1:
+        return lambda winner, neighbourhood_size: _neighbourhood_1d(winner, n_nodes, neighbourhood_size, circular)
+    elif dim == 2:
+        return lambda winner, neighbourhood_size: _neighbourhood_2d(winner, int(math.sqrt(n_nodes)), neighbourhood_size)
 
 
 class SOM:
-    """Self-organizing map with 1D arrangement of nodes."""
+    """Self-organizing map. Support for 1D or 2D grid of nodes."""
 
-    def __init__(self, n_nodes, learning_rate, n_epochs, init_nb_size, circular=False, distance='euclidean',
-                 verbose=False):
+    def __init__(self, dim, n_nodes, learning_rate, n_epochs, init_nb_size, circular=False, distance='euclidean'):
         self.n_nodes = n_nodes
         self.learning_rate = learning_rate
         self.n_epochs = n_epochs
         self._init_nb_size = init_nb_size  # initial neighbourhood size
-        self._circular = circular
-        self._distance = _select_distance(distance)
-        self._verbose = verbose
+        self.dim = dim
+        self.circular = circular
         self.weights = None
+        self._distance = _select_distance(distance)
+        self._neighbourhood = _select_neighbourhood(dim, n_nodes, circular)
 
     def learn(self, patterns):
         """Learn map from patterns."""
-        self.weights = np.random.rand(self.n_nodes, patterns.shape[1])
+        self._init_weights(patterns)
         for i in range(self.n_epochs):
             for p in patterns:
                 winner = self.winner(p)
-                neighbourhood = self._neighbourhood(winner, i)
+                neighbourhood_size = round(self._init_nb_size / self.n_epochs * (self.n_epochs - i))
+                neighbourhood = self._neighbourhood(winner, neighbourhood_size)
                 self._update_weights(p, neighbourhood)
 
     def winner(self, pattern):
         """Get the index of the winner node."""
-        distances = [self._distance(pattern, w) for w in self.weights]
-        winner = distances.index(min(distances))
+        if self.dim == 1:
+            distances = [self._distance(pattern, w) for w in self.weights]
+            winner = distances.index(min(distances))
+        elif self.dim == 2:
+            distances = np.array([[self._distance(pattern, self.weights[i, j]) for j in range(self.weights.shape[1])]
+                                  for i in range(self.weights.shape[0])])
+            winner = np.where(distances == np.amin(distances))
+            winner = (winner[0][0], winner[1][0])   # take the first one
         return winner
 
-    def _neighbourhood(self, node, epoch):
-        """Get neighbourhood, represented as indexes, of a node. The node itself is included."""
-        neighbourhood_size = round(self._init_nb_size / self.n_epochs * (self.n_epochs - epoch))
-        idx_min, idx_max = math.ceil(node - neighbourhood_size), math.floor(node + neighbourhood_size)
-        if self._circular:
-            if idx_max >= self.n_nodes:
-                neighbourhood = np.arange(idx_min, self.n_nodes)                        # [idx_min, n_nodes-1] plus...
-                idx_max %= self.n_nodes
-                neighbourhood = np.concatenate((neighbourhood, np.arange(idx_max+1)))   # ... [0, idx_max % n_nodes]
-            else:
-                neighbourhood = np.arange(idx_min, idx_max + 1)
-        else:
-            if idx_min < 0:
-                idx_min = 0
-            if idx_max >= self.n_nodes:
-                idx_max = self.n_nodes-1
-            neighbourhood = np.arange(idx_min, idx_max + 1)
-        if self._verbose:
-            print('epoch:', epoch)
-            print('winner:', node)
-            print('neighbourhood size:', neighbourhood_size)
-            print('range:', idx_min, idx_max)
-            print('effective neighbourhood size:', len(neighbourhood))
-            print('effective neighbourhood:', neighbourhood)
-            print()
-        return neighbourhood
-
     def _update_weights(self, pattern, neighbourhood):
-        """Move nodes a bit closer to the pattern."""
-        self.weights[neighbourhood] += self.learning_rate * (pattern - self.weights[neighbourhood])
+        """Move nodes in the neighbourhood a bit closer to the pattern."""
+        if self.dim == 1:
+            self.weights[neighbourhood] += self.learning_rate * (pattern - self.weights[neighbourhood])
+        elif self.dim == 2:
+            self.weights[neighbourhood[:, 0], neighbourhood[:, 1]] += \
+                self.learning_rate * (pattern - self.weights[neighbourhood[:, 0], neighbourhood[:, 1]])
+
+    def _init_weights(self, patterns):
+        """Init the weight grid depending on the dimensionality of the output space."""
+        if self.dim == 1:
+            self.weights = np.random.rand(self.n_nodes, patterns.shape[1])
+        elif self.dim == 2:
+            side = int(math.sqrt(self.n_nodes))
+            self.weights = np.random.rand(side, side, patterns.shape[1])
--- Lab 2/lab2/part2.py
+++ Lab 2/lab2/part2.py
@@ -5,36 +5,36 @@ from SOM import SOM
 
 def load_animals(filename_attributes, filename_names):
     with open(filename_attributes) as f:
-        patterns = f.read()
-    patterns = np.array([int(p) for p in patterns.split(',')]).reshape(-1, 84)
+        attributes = f.read()
+    attributes = np.array([int(a) for a in attributes.split(',')]).reshape(-1, 84)
     with open(filename_names) as f:
         names = f.readlines()
-    names = [n.replace("'", "").replace('\t', '').replace('\n', '') for n in names]     # remove ', \t, \n
-    return patterns, names
+    names = [n.strip().strip("'").strip() for n in names]  # remove ', \t, \n
+    return attributes, names
 
 
-def sort_animals(som, patterns, names):
-    idx = list(range(patterns.shape[0]))            # indexes of animals
-    winners = [som.winner(p) for p in patterns]
+def sort_animals(som, attributes, names):
+    idx = list(range(attributes.shape[0]))  # indexes of animals
+    winners = [som.winner(a) for a in attributes]
     aux = zip(idx, winners)
-    aux = sorted(aux, key=lambda x: x[1])           # sort by winner
+    aux = sorted(aux, key=lambda x: x[1])  # sort by winner
     idx = [i[0] for i in aux]
     names = [names[i] for i in idx]
-    return names                                    # animals in natural order
+    return names  # animals in natural order
 
 
 def load_cities(filename):
     with open(filename) as f:
-        patterns = f.readlines()
-    patterns = np.array([[float(x.replace(' ', '').replace(';', '').replace('\n', '')) for x in p.split(',')]
-                         for p in patterns if p[0] != '%' and p[0] != '\n'])
-    return patterns
+        cities = f.readlines()
+    cities = np.array([[float(x.strip().strip(';')) for x in c.split(',')]
+                       for c in cities if c[0] != '%' and c[0] != '\n'])
+    return cities
 
 
-def plot_tour(som, patterns):
-    plt.scatter(patterns[:, 0], patterns[:, 1], c='y', label='city')
+def plot_tour(som, cities):
     # we want a cyclic curve, so we add the first point at the end (the last point gets connected to the first one)
     weights = np.concatenate((som.weights, som.weights[0].reshape(1, -1)), axis=0)
+    plt.scatter(cities[:, 0], cities[:, 1], c='y', label='city')
     plt.plot(weights[:, 0], weights[:, 1], 'b-', label='tour')
     plt.plot(weights[:, 0], weights[:, 1], 'ro', label='tour')
     plt.legend()
@@ -45,25 +45,100 @@ def compute_tour_length(som):
     tour_length = 0
     for i in range(som.n_nodes):
         x = som.weights[i]
-        y = som.weights[(i+1) % som.n_nodes]
-        tour_length += np.linalg.norm(x-y)
+        y = som.weights[(i + 1) % som.n_nodes]
+        tour_length += np.linalg.norm(x - y)
     return tour_length
+
+
+def load_mp_info(filename):
+    with open(filename) as f:
+        values = f.readlines()
+    values = [int(v.strip()) for v in values if v[0] != '%' and v.strip()]
+    return values
+
+
+def load_mp(filename_votes, filename_parties, filename_genders, filename_districts):
+    with open(filename_votes) as f:
+        votes = f.read()
+    votes = np.array([float(p) for p in votes.split(',')]).reshape(-1, 31)
+    parties = load_mp_info(filename_parties)
+    genders = load_mp_info(filename_genders)
+    districts = load_mp_info(filename_districts)
+    return votes, parties, genders, districts
+
+
+def plot_mp_colored(winners, colors, side, title, labels=None):
+    # colors
+    winner_colors = [[[] for j in range(side)] for i in range(side)]
+    for i in range(len(colors)):
+        winner = winners[i]
+        winner_colors[winner[0]][winner[1]].append(colors[i])
+    for i in range(side):
+        for j in range(side):
+            if len(winner_colors[i][j]) != 0:
+                winner_colors[i][j] = sum(winner_colors[i][j]) / len(winner_colors[i][j])
+            else:
+                winner_colors[i][j] = 0
+    plt.imshow(winner_colors)
+    plt.axis('off')
+    plt.title(title)
+
+    # texts
+    if labels is not None:
+        colors = [c-min(colors) for c in colors]    # start from 0 to index correctly the labels
+        winner_labels = [[[0 for k in range(len(labels))] for j in range(side)] for i in range(side)]
+        for i in range(len(colors)):    # count
+            winner = winners[i]
+            winner_labels[winner[0]][winner[1]][colors[i]] += 1
+        for i in range(side):
+            for j in range(side):
+                label = ''
+                for k in range(len(labels)):
+                    if winner_labels[i][j][k] > 0:
+                        label += str(winner_labels[i][j][k]) + ' ' + labels[k] + '\n'
+                plt.text(j, i, label, fontsize=5, ha='center', va='center')
+    plt.show()
+
+
+def plot_mp(som, votes, parties, genders, districts):
+    side = som.weights.shape[0]
+    winners = [som.winner(v) for v in votes]
+
+    # parties
+    labels = ['no party', 'm', 'fp', 's', 'v', 'mp', 'kd', 'c']
+    plot_mp_colored(winners, parties, side, 'Output space (colored by party)', labels=labels)
+
+    # genders
+    labels = ['male', 'female']
+    genders = [g+1 for g in genders]    # to distinguish the color between nobody and men in the grid
+    plot_mp_colored(winners, genders, side, 'Output space (colored by gender)', labels=labels)
+
+    # districts
+    # labels = [str(d) for d in districts]
+    # plot_mp_colored(winners, districts, side, labels=labels)
+    plot_mp_colored(winners, districts, side, 'Output space (colored by district)')
 
 
 np.random.seed(1)
 
 # 4.1 - Animals
-patterns, names = load_animals('datasets/animals.dat', 'datasets/animalnames.txt')
-som = SOM(n_nodes=100, learning_rate=0.2, n_epochs=20, init_nb_size=50, verbose=False)
-som.learn(patterns)
-names = sort_animals(som, patterns, names)
+attributes, names = load_animals('datasets/animals.dat', 'datasets/animalnames.txt')
+som = SOM(dim=1, n_nodes=100, learning_rate=0.2, n_epochs=20, init_nb_size=50)
+som.learn(attributes)
+names = sort_animals(som, attributes, names)
 print('Sorted animals:', names)
 
 # 4.2 - Cyclic tour
-patterns = load_cities('datasets/cities.dat')
-som = SOM(n_nodes=10, learning_rate=0.5, n_epochs=20, init_nb_size=2, circular=True, verbose=False)
-som.learn(patterns)
-plot_tour(som, patterns)
+cities = load_cities('datasets/cities.dat')
+som = SOM(dim=1, n_nodes=10, learning_rate=0.5, n_epochs=20, init_nb_size=2, circular=True)
+som.learn(cities)
+# plot_tour(som, cities)
 tour_length = compute_tour_length(som)
 print('Tour length:', tour_length)
 
+# 4.3 - Votes of MPs
+votes, parties, genders, districts = load_mp('datasets/votes.dat', 'datasets/mpparty.dat',
+                                             'datasets/mpsex.dat', 'datasets/mpdistrict.dat')
+som = SOM(dim=2, n_nodes=100, learning_rate=0.5, n_epochs=50, init_nb_size=10)
+som.learn(votes)
+plot_mp(som, votes, parties, genders, districts)
