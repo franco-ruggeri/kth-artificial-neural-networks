34a7ef0e28fdad45c5e9dd0df3c29dfbd0bdad36
unknown <fdaniels@kth.se>
2020-02-16 18:51:06 +0100

    older version, specifically for task 3.3

--- Lab 3/lab3/utility.py
+++ Lab 3/lab3/utility.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 
 def check_stability(hn, patterns):
     for pattern in patterns:
-        result = hn.recall(pattern, synchronous=True, max_iters=1)
+        result = hn.recall(pattern, update_rule='synch', max_iters=1)
         if not result[2] or any(result[0] != pattern):
             return False
     return True
